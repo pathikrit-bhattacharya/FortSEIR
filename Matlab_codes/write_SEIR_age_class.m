ebbbaaca0cdd5ba68c3b1c9f4d69f85087ac4259
pathikrit-bhattacharya <path_geoalum@alumni.princeton.edu>
2020-04-06 23:15:00 +0530

    Update write_SEIR_age_class.m

--- Matlab_codes/write_SEIR_age_class.m
+++ Matlab_codes/write_SEIR_age_class.m
@@ -18,7 +18,7 @@ t_tot   = 365; % How long will the simulation run
 N_tot   = 1.37e9; % Total poulation
 rho = [0.8*ones(4,1);0.6*ones(10,1);1;1];
 alpha = 0.25; % Prob that infected asymptomatic infects
-beta  = 0.1d0; % Prob. that infectious transmits disease to susceptible
+beta  = 0.8d0; % Prob. that infectious transmits disease to susceptible
 kappa = ones(16,1)./Incub_dur;
 gamma = ones(16,1)./Infec_dur;
 mu_n  = zeros(16,1); % 0.007*ones(16,1)/365; % scale to per day
@@ -28,8 +28,8 @@ w_w = [0.0*ones(4,1);0.2*ones(10,1);0.2*ones(2,1)]; % Effect of lockdown at work
 w_h = [1.8*ones(4,1);1.5*ones(10,1);ones(2,1)]; % Effect of lockdown at home
 w_o = [0.1*ones(14,1);0.0*ones(2,1)]; % Effect of lockdown at other places
 Nis  = load([pwd '\input\India_age_data.txt']); % Population fractions in age groups
-I0   = 2000*Nis/N_tot; % Number of infected initially, populated as population age fractions
-S0  = 1-2*I0; % Initial number of susceptible in age group, E0 = I0
+I0   = 200*Nis/N_tot; % Number of infected initially, populated as population age fractions
+S0  = 1-1.25*I0; % Initial number of susceptible in age group, E0 = I0
 A = [rho,kappa,gamma,mu_n,mu_d,w_s,w_w,w_h,w_o,Nis,I0,S0];
 t_l1    = 20; % Lockdown onset
 t_l2    = 41; % Lockdown stop
